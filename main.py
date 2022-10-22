672efcedf7c25981aec9fc8b0eb7e4d2c8ae7324
wiltv <71708561+wiltv@users.noreply.github.com>
2022-10-22 19:31:59 -0400

    Main

---
+++ main.py
@@ -0,0 +1,16 @@
+import kivy
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+
+
+class NANA(App):
+    def build(self):
+        #return Label(text = "No jodaaa")
+        boton = Button(text = "Presioname", font_size=30, size_hint=(0.3, 0.1), pos=(250,250), background_color=("blue"))
+        return boton
+
+
+
+if __name__ == "__main__":
+    NANA().run()
