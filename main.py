ec10f1924857937c55880dc576896fc43f51acbb
Angélica Tezara Oberto <61010107+angelicaoberto@users.noreply.github.com>
2022-10-22 19:17:35 -0400

    prueba 1

---
+++ main.py
@@ -0,0 +1,17 @@
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.uix.image import Image
+from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
+
+class SayHello(App):
+    def build(self):
+        self.window = GridLayout()
+        self.window.cols = 1
+        #add widgets to window
+        self.window.add_widget(Label(text="Hola, soy Nana"))
+        return self.window
+
+if __name__ == "__main__":
+    SayHello().run()
