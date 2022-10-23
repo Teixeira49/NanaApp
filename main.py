4843665aa5f3ec04788013a1e000572e3e6b7c0f
Angélica Tezara Oberto <61010107+angelicaoberto@users.noreply.github.com>
2022-10-22 23:53:29 -0400

    botón micrófono reconocimiento de voz traducción voz a texto y viceversa reproducir canciones en youtube

--- main.py
+++ main.py
@@ -1,17 +1,59 @@
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.widget import Widget
 from kivy.uix.label import Label
 from kivy.uix.image import Image
 from kivy.uix.button import Button
+from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.textinput import TextInput
+import speech_recognition as sr
+import pyttsx3
+import pywhatkit
 
-class SayHello(App):
+class MyLayout(Widget):
+    def callback(self):
+
+        listener = sr.Recognizer()
+        voice_id = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_ES-MX_SABINA_11.0"
+        nanavoice = pyttsx3.init()
+        nanavoice.setProperty("voice", voice_id)
+        nanavoice.say('Hola, soy Nana. ¿Qué puedo hacer por ti?')
+        nanavoice.runAndWait()
+        def talk(text):
+            nanavoice.say(text)
+            nanavoice.runAndWait()
+
+        def take_command():
+
+            try:
+                with sr.Microphone() as source:
+                    print("Escuchando...")
+                    listener.adjust_for_ambient_noise(source)
+                    voice = listener.listen(source)
+                    command = listener.recognize_google(voice)
+                    command = command.lower()
+                    print(command)
+            except:
+                pass
+            return command
+
+        def run_nana():
+            command = take_command()
+            if 'reproduce' in command:
+                song = command.replace('reproduce', '')
+                talk('Reproduciendo ' + song)
+                pywhatkit.playonyt(song)
+
+
+        run_nana()
+
+class NanaApp(App):
     def build(self):
-        self.window = GridLayout()
-        self.window.cols = 1
-        #add widgets to window
-        self.window.add_widget(Label(text="Hola, soy Nana"))
-        return self.window
+        return MyLayout()
+
+
+
+
 
 if __name__ == "__main__":
-    SayHello().run()
+    NanaApp().run()
