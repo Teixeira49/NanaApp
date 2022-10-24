b2093851c1ef72b7b6bc1b0e80ecce148045e5d0
Angélica Tezara Oberto <61010107+angelicaoberto@users.noreply.github.com>
2022-10-24 10:54:38 -0400

    botón micrófono, reconocimiento de voz, traducción voz a texto y viceversa, reproducir canciones en youtube, mandar mensajes en whatsappp (bug arreglado)

--- main.py
+++ main.py
@@ -1,24 +1,27 @@
 from kivy.app import App
-from kivy.uix.gridlayout import GridLayout
 from kivy.uix.widget import Widget
 from kivy.uix.label import Label
 from kivy.uix.image import Image
 from kivy.uix.button import Button
-from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.textinput import TextInput
+from kivy.uix.behaviors import ButtonBehavior
+from kivy.lang import Builder
 import speech_recognition as sr
 import pyttsx3
 import pywhatkit
 
 class MyLayout(Widget):
+
     def callback(self):
 
         listener = sr.Recognizer()
         voice_id = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_ES-MX_SABINA_11.0"
         nanavoice = pyttsx3.init()
         nanavoice.setProperty("voice", voice_id)
+        nanavoice.setProperty('rate', 150)
         nanavoice.say('Hola, soy Nana. ¿Qué puedo hacer por ti?')
         nanavoice.runAndWait()
+
         def talk(text):
             nanavoice.say(text)
             nanavoice.runAndWait()
@@ -38,19 +41,18 @@ class MyLayout(Widget):
             return command
 
         def run_nana():
-            command = take_command()
-            if 'reproduce' in command:
-                song = command.replace('reproduce', '')
+            order = take_command()
+            if 'reproduce' in order:
+                song = order.replace('reproduce', '')
                 talk('Reproduciendo ' + song)
                 pywhatkit.playonyt(song)
 
-            if 'mensaje' in command:
+            if 'mensaje' in order:
                 #se debe implementar una opción que deje buscar contactos por nombre, y extraer el número de ahí
                 talk('¿Qué quieres decirle?')
                 mensaje = take_command()
-                #número de angélica como prueba
-                #debería mandar el mensaje automáticamente
-                pywhatkit.sendwhatmsg_instantly('+584120999401', mensaje, 3, True, 3)
+                #número de angelica como prueba
+                pywhatkit.sendwhatmsg_instantly('+584120999401', mensaje, 11, True, 6)
 
 
         run_nana()
@@ -58,9 +60,6 @@ class MyLayout(Widget):
 class NanaApp(App):
     def build(self):
         return MyLayout()
-
-
-
 
 
 if __name__ == "__main__":
