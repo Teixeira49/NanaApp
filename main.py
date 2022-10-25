ad74a6091828cbcfebe074f3e8eb27c94593e663
Javier Teixeira <ja.teixeira@correo.unimet.edu.ve>
2022-10-25 19:26:13 -0400

    modificado el main, creado archivo funciones para centralizar la ejecucion, por ahora algo inoptimo, pero lo reparare, añadida ademas una funcion secundaria

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
-import kivy
 from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.button import Button
+from kivy.uix.widget import Widget
+from kivymd.app import MDApp
+from kivymd.uix.screen import MDScreen
+from kivy.lang import Builder
+from pyautogui import sleep
+import speech_recognition as sr
+import pyttsx3
+import pywhatkit
+from time import sleep
+# archivos
+import function as f
 
+class NanaApp(MDApp):
 
-class NANA(App):
     def build(self):
-        #return
-        boton = Button(text = "click mee", font_size=30, size_hint=(0.3, 0.1), pos=(250,250), background_color=("red"))
-        return boton
+        self.theme_cls.theme_style = "Dark"
+        self.theme_cls.primary_palette = "Green"
+        return
 
-
+    def callback(self):
+        print("Preparando escucha...")
+        f.talk('Hola, soy Nana. ¿Qué puedo hacer por ti?')
+        order = f.take_command()
+        f.run_nana(order)
 
 if __name__ == "__main__":
-    NANA().run()
+    NanaApp().run()
