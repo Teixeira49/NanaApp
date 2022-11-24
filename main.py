d43b619fb103993eecd8483bc339babd9e16ddcd
Angélica Tezara Oberto <61010107+angelicaoberto@users.noreply.github.com>
2022-11-23 20:34:08 -0400

    implementación recordatorios con fecha y hora

--- main.py
+++ main.py
@@ -6,18 +6,23 @@ from kivymd.app import MDApp
 from kivymd.uix.screen import Screen
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
+from kivymd.uix.pickers import MDDatePicker
+from kivymd.uix.pickers import MDTimePicker
+from kivymd.uix.list import MDList
+from kivy.uix.scrollview import ScrollView
+from kivymd.uix.list import MDList, ThreeLineListItem
 # ============================================================================== Librerias Kivy ===========
 import function as f
 import json
 import random
 import webbrowser
-from tkinter import *
-#from tkcalendar import Calendar 
 # ============================================================================== Archivos =================
 #  >> Carga de archivos:
 # ---------------------------------------------------------------------------------------------------------
 with open('data_talk.json', 'r') as file:
     data = json.load(file)
+
+
 # ---------------------------------------------------------------------------------------------------------
 #  >> Paneles
 # ---------------------------------------------------------------------------------------------------------
@@ -29,8 +34,11 @@ class CalendarScreen(Screen):
     pass
 class SmsScreen(Screen):
     pass
-class NanaApp(MDApp):
 
+class AppointmentScreen(Screen):
+    pass
+class NanaApp(MDApp):
+    eventos = {}
     def build(self):
         self.theme_cls.primary_palette = "Green"
         return
@@ -46,6 +54,7 @@ class NanaApp(MDApp):
         f.talk(data["query"][random.randint(0, len(data["query"]) - 1)])
         f.run_nana(order)
 
+
     def profile(self):
         webbrowser.open_new_tab(
             'https://www.google.com/search?q=como+crear+perfil&client=opera-gx&sxsrf=ALiCzsbEvkZRB-XStGjrA7B8OmgWvB8rdg%3A1667780750279&ei=jlBoY97aEJyZwbkPuKOL-Ac&ved=0ahUKEwje2bu555r7AhWcTDABHbjRAn8Q4dUDCA4&uact=5&oq=como+crear+perfil&gs_lcp=Cgxnd3Mtd2l6LXNlcnAQAzIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQ6CggAEEcQ1gQQsAM6BwgAELADEEM6DQgAEOQCENYEELADGAE6BAgjECc6CggAEIAEEIcCEBQ6CAgAELEDEIMBOgUIABCxAzoLCAAQgAQQsQMQgwE6CAgAEIAEELEDOggIABCABBDJA0oECE0YAUoECEEYAEoECEYYAVCCzgNYnuwDYLPuA2gCcAF4AoABvgWIAY0gkgENMi44LjIuMy4wLjEuMZgBAKABAcgBEcABAdoBBggBEAEYCQ&sclient=gws-wiz-serp')
@@ -53,38 +62,6 @@ class NanaApp(MDApp):
     def call(self):
         webbrowser.open_new_tab(
             'https://www.google.com/search?q=como+llamar&client=opera-gx&sxsrf=ALiCzsbpqNiZ73J-mRKVXwcrIKb2JxxmEw%3A1667780733762&ei=fVBoY7qXLsyGwbkPn-CfyAI&ved=0ahUKEwj6ycux55r7AhVMQzABHR_wBykQ4dUDCA4&uact=5&oq=como+llamar&gs_lcp=Cgxnd3Mtd2l6LXNlcnAQAzIKCAAQgAQQhwIQFDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDoHCCMQ6gIQJzoECCMQJzoKCC4QxwEQ0QMQQzoICAAQgAQQsQM6CwgAEIAEELEDEIMBOg4ILhCABBCxAxDHARDRAzoRCC4QgAQQsQMQgwEQxwEQ0QM6DQguEMcBENEDENQCEEM6BwgAEIAEEAM6CAgAELEDEIMBOgQIABBDOgsIABCABBCxAxDJAzoLCC4QgAQQsQMQgwE6CAguEIAEENQCSgQITRgBSgQIQRgASgQIRhgAUABY3g5g5RJoAXABeACAAe8BiAGmDZIBBTAuOC4zmAEAoAEBsAEKwAEB&sclient=gws-wiz-serp')
-
-    
-    def agendar(self):
-        root = Tk() 
-        root.geometry("400x400")  
-        cal = Calendar(root, selectmode = 'day', year = 2022, month = 11, day = 24)   
-        cal.pack(pady = 20) 
-        
-        def grad_date(): 
-            date.config(text = "Selected Date is: " + cal.get_date()) 
-            var = cal.get_date()
-            with open("fecha.txt", "a") as file:
-                file.write(var)
-            root.destroy()
-            
-        
-        Button(root, text = "Elegir fecha", command = grad_date).pack(pady = 20) 
-        
-        date = Label(root, text = "") 
-        date.pack(pady = 20)
-
-        
-        root.mainloop()
-
-
-        with open("fecha.txt", "a") as file:
-                file.write(f" {self.root.get_screen('CalendarScreen').ids.recordatorio.text}")
-                file.write("\n")
-        self.root.get_screen('CalendarScreen').ids.recordatorio.text = ""
-
-
-
 
     def search(self):
         order = self.root.get_screen('MDScreen').ids.data.text
@@ -163,7 +140,65 @@ class NanaApp(MDApp):
         f.send_nana(diccionario, persona, mensaje)
         self.root.get_screen('SmsScreen').ids.people.text = ""
         self.root.get_screen('SmsScreen').ids.msg.text = ""
+
+    def on_save(self, instance, value, date_range):
+        self.root.get_screen('CalendarScreen').ids.date_label.text = f'{str(date_range[0])} / {str(date_range[-1])}'
+
+
+    def on_cancel(self, instance, value):
+        self.root.get_screen('CalendarScreen').ids.date_label.text = ""
+
+    def show_date_picker(self):
+        date_dialog = MDDatePicker(mode="range")
+        date_dialog.bind(on_save=self.on_save, on_cancel=self.on_cancel)
+        date_dialog.open()
+
+    # Get Time
+    def get_time(self, instance, time):
+        self.root.get_screen('CalendarScreen').ids.time_label.text = str(time)
+
+    # Cancel
+    def on_cancel(self, instance, time):
+        self.root.get_screen('CalendarScreen').ids.time_label.text = ""
+
+    def show_time_picker(self):
+        from datetime import datetime
+
+        # Define default time
+        default_time = datetime.strptime("4:20:00", '%H:%M:%S').time()
+
+        time_dialog = MDTimePicker()
+        # Set default Time
+        time_dialog.set_time(default_time)
+        time_dialog.bind(on_cancel=self.on_cancel, time=self.get_time)
+        time_dialog.open()
+
+
+    def save_event(self):
+        time = self.root.get_screen('CalendarScreen').ids.time_label.text
+        date = self.root.get_screen('CalendarScreen').ids.date_label.text
+        value = self.root.get_screen('CalendarScreen').ids.date_name.text
+        if date not in self.eventos.keys():
+            self.eventos[date] = {}
+            self.eventos[date][time] = value
+        else:
+            self.eventos[date][time] = value
+        print(self.eventos)
+
+        self.root.get_screen('CalendarScreen').ids.time_label.text = ""
+        self.root.get_screen('CalendarScreen').ids.date_label.text = ""
+        self.root.get_screen('CalendarScreen').ids.date_name.text = ""
+
+    def appnt_list(self):
+        for i in list(self.eventos.keys()):
+            for y in list(self.eventos[i].keys()):
+                items = ThreeLineListItem(text=self.eventos[i][y],
+                                          secondary_text=i,
+                                          tertiary_text=y)
+        self.root.get_screen('AppointmentScreen').ids.container.add_widget(items)
+
 # ---------------------------------------------------------------------------------------------------------
 if __name__ == "__main__":
     NanaApp().run()
+    #NanaApp().show_notification("hey user", "take a break now")
 # ---------------------------------------------------------------------------------------------------------
