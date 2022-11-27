2ecd977d045181e9be49974bdceda9dc2dfdb934
MariaKFerrer <116607321+MariaKFerrer@users.noreply.github.com>
2022-11-27 01:16:11 -0400

    notificaciones y contactos con kvdroid
    implemente lo de kvdroid pero no sabia como guardar el diccionario de los contactos en el json o en el firebase que implemento angelica

---
+++ function.py
@@ -0,0 +1,209 @@
+# =========================================================================================================
+#from re import X
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivymd.app import MDApp
+from kivymd.uix.screen import MDScreen
+from kivy.lang import Builder
+from googlesearch import search
+#from googlesearch.googlesearch import GoogleSearch #para solucionar el error de search en mi compu
+#import webbrowser
+from plyer import notification
+from kvdroid.tools.contact import get_contact_details
+from kvdroid.jclass.android.graphics import Color
+from kvdroid.tools.notification import create_notification
+from kvdroid.tools import get_resource
+# ============================================================================== Librerias Kivy ===========
+import pyautogui as pg
+import speech_recognition as sr
+import pyttsx3
+import pywhatkit
+import random
+import requests
+import json
+# ============================================================================== Librerias ================
+#  >> Carga de archivos
+# ---------------------------------------------------------------------------------------------------------
+with open('data_talk.json', 'r') as file:
+    data = json.load(file)
+
+# =========================================================================================================
+#  >> Funcionamiento del COMMAND
+# ---------------------------------------------------------------------------------------------------------
+def take_command():                                     # Peticion de la orden
+    listener = sr.Recognizer()
+    command = ''
+    try:
+        with sr.Microphone() as source:
+            print("Escuchando...")
+            listener.adjust_for_ambient_noise(source)
+            voice = listener.listen(source)
+            command = listener.recognize_google(voice)
+            command = command.lower()
+            print(command)
+        if command == "":
+            talk("No he escuchado nada, porfavor pulse e intente nuevamente")
+    except:
+        conmand = "None"
+    return command
+# ---------------------------------------------------------------------------------------------------------
+def run_nana(order):                                    # Realizacion de la orden
+    now = dt.now()
+    if 'reproduce' in order.lower(): # optimizar porfavor
+        song = order.replace('reproduce', '')
+        talk('Reproduciendo ' + song)
+        pywhatkit.playonyt(song)
+    elif 'mensaje' in order.lower():
+        # se debe implementar una opción que deje buscar contactos por nombre, y extraer el número de ahí
+        talk('¿Qué quieres decirle?')
+        mensaje = take_command()
+        # número de angelica como prueba
+        pywhatkit.sendwhatmsg_instantly('+584120999401', mensaje, 11, True, 6)
+    elif "hoy" in order or "fecha" in order.lower():
+        talk(f"Hoy estamos a {now.day} de {now.month} del año {now.year}")
+    elif "hora" in order.lower():
+        talk(f"Son las {now.hour} horas con {now.minute} minutos")
+    elif "recordatorio" in order.lower():
+        talk("¿Que desea recordar?")
+        mensaje = take_command()
+    elif "hipertenso" in order.lower(): # Modificar cuando tengamos la BD implementada # ["hipertenso", "hipertensa", "hipertension"]
+        #notify()
+        salud("hipertenso") # https://www.medicalnewstoday.com/articles/es/alimentos-a-evitar-con-presion-arterial-alta#alimentos-salados
+    elif  "diabetes" in order.lower(): #["diabetes", "diabetico", "diabetica", "diabete"]
+        salud("diabetes")
+    elif "clima" in order.lower() or "tiempo" in order.lower():
+        dat = ""
+        if "en " in order:
+            dat = "London" # por ahora
+        else:
+            dat = "Caracas"
+        weather(data["weathers"]["urls"]["weather"], data["weathers"]["urls"]["location"], dat, data["weathers"]["key"])
+    elif "repetir" in order.lower() or "repite" in order.lower():
+        repeat(order.replace("repetir", "").replace("repite", ""))
+    elif "cuentame" in order.lower() or "quien soy" in order.lower() or "chiste" in order.lower() or "cuento" in order.lower() or "historia" in order.lower() or "aforismo" in order.lower() or "frase" in order.lower():
+        tell(order)
+    else:           # realizara la consulta automaticamente en el internet
+        talk("De " + order + " " + data["result"][random.randint(0, len(data["result"])-1)])
+        lista = GoogleSearch().search(order)
+        valor = ""
+        for i in lista:
+            if valor == "":
+                valor = i
+                break
+        webbrowser.open_new_tab(valor)
+# =========================================================================================================
+#   >> Funciones de la APP
+# ---------------------------------------------------------------------------------------------------------
+def weather(url_t, url_loc, city, key):         # Consultar clima
+    try:
+        response = requests.request("GET", (url_loc.replace("city_name", city).replace("limit_d", "1").replace("appid_r", key)))
+        lat, lon = response.json()[-1]["lat"], response.json()[-1]["lon"]
+        responses = requests.request("GET", (url_t.replace("lat_r", f"{lat}").replace("lon_r", f"{lon}").replace("appid_r", key)))
+        x = responses.json()
+        clima = x["weather"][0]["main"]
+        desc = x["weather"][0]["description"]
+        temp = x["main"]["temp"]
+        humb = x["main"]["humidity"]
+        v_sp = x["wind"]["speed"]
+        direc = x["wind"]["deg"]
+        numb = x["clouds"]["all"]
+        # CONVERTIR EN NOTIFICACION
+        talk(f"Clima: {clima}, Descripcion: {desc}, Temperatura promedio: {temp}C°, Humedad: {humb}, viento a velocidad de: {v_sp} sopla en direccion de {direc} con nubosidad de {numb} puntos")
+    except:
+        print("Error de busqueda")
+        weather(url_t, url_loc, "Caracas", key)
+# ---------------------------------------------------------------------------------------------------------
+def send_nana(json,persona, mensaje):           # Enviar un mensaje
+    now = dt.now()
+    for k,v in json.items():
+        if persona == k:
+            pywhatkit.sendwhatmsg_instantly(v, mensaje, 30, True, 22)
+            pg.press("enter")
+# ---------------------------------------------------------------------------------------------------------
+def talk(text):
+    nanavoice = pyttsx3.init()
+    nanavoice.setProperty('rate', 150)
+    nanavoice.setProperty("voice", "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_ES-MX_SABINA_11.0")
+    print(text)
+    nanavoice.say(text)
+    nanavoice.runAndWait()
+# ---------------------------------------------------------------------------------------------------------
+'''def notify():
+        Notification().open(
+            title="Recordatorio",
+            icon="./Images/logo blanco.png",
+            message="Los granos integrales son preferibles a los productos de harina blanca o pasta para la hipertensión",
+            timeout=5,
+        )'''
+# ---------------------------------------------------------------------------------------------------------
+def tell(x):
+    if "quien soy" in x:
+        now = dt.now()
+        y,z,w = (int(str(data["perfil"]["cumpleaos"][0:1]))), (int(str(data["perfil"]["cumpleaos"][3:4]))), ((now.year) - int(str(data["perfil"]["cumpleaos"][6:10])))
+        if now.day >= y and now.month >= z:
+            w += 1
+        talk("Tu eres: "+ data["perfil"]["Nombre"]+" "+ data["perfil"]["Apellido"]+ ". Naciste el: "+ data["perfil"]["cumpleaos"]+ ", por lo que tienes: " + f"{w-1} años")
+    elif "chiste" in x:
+        talk(data["tell_phrase"]["comedy"][random.randint(0, len(data["tell_phrase"]["comedy"]))])
+    elif "historia" in x or "cuento" in x:
+        talk(data["tell_phrase"]["m_story"][random.randint(0, len(data["tell_phrase"]["m_story"]))])
+    elif "aforismo" in x or "frase" in x:
+        talk(data["tell_phrase"]["phrase"][random.randint(0, len(data["tell_phrase"]["phrase"]))])
+# ---------------------------------------------------------------------------------------------------------
+def repeat(frase):
+    if frase != "":
+        sleep(2)
+        talk(frase)
+    else:
+        talk(data["error"][0][random.randint(0, len(data["error"][0]))], ", ",data["error"][1][random.randint(0, len(data["error"][1]))])
+# ---------------------------------------------------------------------------------------------------------
+def notify_fech():
+    now = dt.now()
+    if data["perfil"]["cumpleaos"] == f"{now.day}-{now.month}-{now.year}":
+        x,y = ((now.year) - data["perfil"]["cumpleaos"][6:10]), data["perfil"]["Nombre"]
+        talk(f"FELICIDADES {y} , HOY ES TU CUMPLEAÑOS NUMERO {x}")# AÑADIR tambien como RECORDATORIO
+# ---------------------------------------------------------------------------------------------------------
+def salud(illness):
+    tell = ["", ""]
+    for i in data["salud"]["frase"]:
+        tell[0] += i[random.randint(0, len(i)-1)] + " "
+    tell[0] += "."
+    if illness == "diabetes":
+        tell[1] = "dbetes"
+    else:
+        tell[1] = "hipert"
+    for i in range(1, len(data["salud"][tell[1]])+1):
+        temp = data["salud"][tell[1]][i-1]
+        tell[0] += f"{i}, {temp}. "
+    talk(tell[0])
+# ---------------------------------------------------------------------------------------------------------
+def show_notification(self, tittle, message): #usando plyer
+    notification.notify(
+        title=tittle,
+        message=message,
+        timeout=10,
+        app_name="Nana",
+        app_icon="./Images/LOGO NANA.ico"
+    )
+
+def show_notificacions(self, tittle, message): #usando kvdroid 
+    create_notification(
+        small_icon="./Images/LOGO NANA.ico",  # app icon
+        channel_id="1", title="You have a message",
+        text="hi, just wanted to check on you",
+        ids=1, channel_name=f"ch1",  # no se de que se trata esto
+        large_icon="assets/image.png",  # creo que esto es el icono que va al lado del texto
+        expandable=True,
+        small_icon_color=Color().rgb(0x00, 0xCC, 0x00),  # 0x00 0xCC 0x00 para lightgreen 00CC00
+        big_picture="assets/image.png"  # esto tampoco se que es
+    )
+# ---------------------------------------------------------------------------------------------------------
+phone_book=get_contact_details("phone_book")  # gets a dictionary of all contact both contact name and phone mumbers
+nombres = get_contact_details("names")  # gets a list of all contact names
+tlf = get_contact_details("mobile_no")  # gets a list of all contact phone numbers
+
+def extraer_contactos(self, nombres, tlf):
+    for y in tlf:
+        for i in nombres:
+            contactos[y] = i
+
--- main.py
+++ main.py
@@ -1,43 +1,215 @@
-from kivy.lang import Builder
+# =========================================================================================================
+#from turtle import width
+from kivy.config import Config
+Config.set('kivy', 'keyboard_mode', 'systemandmulti')
+from kivy.app import App
+from kivy.uix.widget import Widget
 from kivymd.app import MDApp
+from kivymd.uix.screen import Screen
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
 from kivymd.uix.pickers import MDDatePicker
+from kivymd.uix.pickers import MDTimePicker
+from kivymd.uix.list import MDList
+from kivy.uix.scrollview import ScrollView
+from kivymd.uix.list import MDList, ThreeLineListItem
+from datetime import date, datetime
+# ============================================================================== Librerias Kivy ===========
+import function as f
+import json
+import random
+import webbrowser
+# ============================================================================== Archivos =================
+#  >> Carga de archivos:
+# ---------------------------------------------------------------------------------------------------------
+with open('data_talk.json', 'r') as file:
+    data = json.load(file)
 
 
-class main(MDApp):
+# ---------------------------------------------------------------------------------------------------------
+#  >> Paneles
+# ---------------------------------------------------------------------------------------------------------
+class MDScreen(Screen):
+    pass
+class PersonScreen(Screen):
+    pass
+class CalendarScreen(Screen):
+    pass
+class SmsScreen(Screen):
+    pass
+
+class AppointmentScreen(Screen):
+    pass
+class NanaApp(MDApp):
+    eventos = {}
     def build(self):
-        self.theme_cls.theme_style = "Light"
         self.theme_cls.primary_palette = "Green"
-        return Builder.load_file('nana.kv')
+        return
+
+    def callback(self):
+        print("Preparando escucha...")
+        word = ""
+        for i in data["Quest"]:
+            x = random.randint(0, len(i) - 1)
+            word += i[x] + " "
+        f.talk(word)
+        order = f.take_command()
+        f.talk(data["query"][random.randint(0, len(data["query"]) - 1)])
+        f.run_nana(order)
+
+
+    def profile(self):
+        webbrowser.open_new_tab(
+            'https://www.google.com/search?q=como+crear+perfil&client=opera-gx&sxsrf=ALiCzsbEvkZRB-XStGjrA7B8OmgWvB8rdg%3A1667780750279&ei=jlBoY97aEJyZwbkPuKOL-Ac&ved=0ahUKEwje2bu555r7AhWcTDABHbjRAn8Q4dUDCA4&uact=5&oq=como+crear+perfil&gs_lcp=Cgxnd3Mtd2l6LXNlcnAQAzIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQ6CggAEEcQ1gQQsAM6BwgAELADEEM6DQgAEOQCENYEELADGAE6BAgjECc6CggAEIAEEIcCEBQ6CAgAELEDEIMBOgUIABCxAzoLCAAQgAQQsQMQgwE6CAgAEIAEELEDOggIABCABBDJA0oECE0YAUoECEEYAEoECEYYAVCCzgNYnuwDYLPuA2gCcAF4AoABvgWIAY0gkgENMi44LjIuMy4wLjEuMZgBAKABAcgBEcABAdoBBggBEAEYCQ&sclient=gws-wiz-serp')
+
+    def call(self):
+        webbrowser.open_new_tab(
+            'https://www.google.com/search?q=como+llamar&client=opera-gx&sxsrf=ALiCzsbpqNiZ73J-mRKVXwcrIKb2JxxmEw%3A1667780733762&ei=fVBoY7qXLsyGwbkPn-CfyAI&ved=0ahUKEwj6ycux55r7AhVMQzABHR_wBykQ4dUDCA4&uact=5&oq=como+llamar&gs_lcp=Cgxnd3Mtd2l6LXNlcnAQAzIKCAAQgAQQhwIQFDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDoHCCMQ6gIQJzoECCMQJzoKCC4QxwEQ0QMQQzoICAAQgAQQsQM6CwgAEIAEELEDEIMBOg4ILhCABBCxAxDHARDRAzoRCC4QgAQQsQMQgwEQxwEQ0QM6DQguEMcBENEDENQCEEM6BwgAEIAEEAM6CAgAELEDEIMBOgQIABBDOgsIABCABBCxAxDJAzoLCC4QgAQQsQMQgwE6CAguEIAEENQCSgQITRgBSgQIQRgASgQIRhgAUABY3g5g5RJoAXABeACAAe8BiAGmDZIBBTAuOC4zmAEAoAEBsAEKwAEB&sclient=gws-wiz-serp')
+
+    def search(self):
+        order = self.root.get_screen('MDScreen').ids.data.text
+        self.root.get_screen('MDScreen').ids.data.text = ''
+        f.run_nana(order)
+
+    def send_people(self):
+        with open('contactos.txt', 'r') as g:
+            lineas = g.readlines()
+            llamar = [l.split() for l in lineas]
+
+            for i in llamar:
+                if i == []:
+                    llamar.remove([])
+            for i in llamar:
+                if len(i[0].split(sep=':')) < 2:
+                    llamar.remove(i)
+            for i in llamar:
+                if len(i) > 1:
+                    new = ""
+                    new += (i[0].split(sep=':'))[1]
+                    cont = 1
+                    for j in i:
+                        new += " "
+                        new += str(i[cont])
+                        cont += 1
+                        if j == i[-2]:
+                            break
+
+                    new2 = ''
+                    new2 += (i[0].split(sep=':'))[0]
+                    new2 += ':'
+                    new2 += new
+
+                    i[0] = new2
+                    while len(i) > 1:
+                        i.pop(-1)
+
+            llamar2 = []
+            for i in llamar:
+                if not i in llamar2:
+                    llamar2.append(i)
+
+            print(llamar2)
+
+            lista_nombre = []
+            lista_tlf = []
+            while len(llamar2) > 0:
+                if list(llamar2[0][0])[0] == 'N' and list(llamar2[1][0])[0] == 'P':
+                    lista_nombre.append((llamar2[0][0]))
+                    lista_tlf.append((llamar2[1][0]))
+                    llamar2.remove(llamar2[0])
+                    llamar2.remove(llamar2[1])
+                else:
+                    llamar2.remove(llamar2[0])
+
+            lista_nombre2 = []
+            for i in lista_nombre:
+                x = i.replace('Name:', "")
+                lista_nombre2.append(x)
+
+            lista_tlf2 = []
+            for i in lista_tlf:
+                x = i.replace('PhoneNumber:', "")
+                lista_tlf2.append(x)
+
+            diccionario = {}
+            cont = 0
+            while cont < len(lista_nombre2):
+                diccionario[lista_nombre2[cont]] = lista_tlf2[cont]
+                cont += 1
+
+
+        persona = self.root.get_screen('SmsScreen').ids.people.text
+        mensaje = self.root.get_screen('SmsScreen').ids.msg.text
+        f.send_nana(diccionario, persona, mensaje)
+        self.root.get_screen('SmsScreen').ids.people.text = ""
+        self.root.get_screen('SmsScreen').ids.msg.text = ""
 
     def on_save(self, instance, value, date_range):
-        #self.root.ids.date_label.text = str(value)
-        self.root.ids.date_label.text = f'{str(date_range[0])} / {str(date_range[-1])}'
-        eventos = {"2000-12-27":"cumple nieto cristian"}
-        evento = self.root.ids.date_name.text
-        self.root.ids.date_name.text = ""
-        eventos[value] = evento
+        self.root.get_screen('CalendarScreen').ids.date_label.text = f'{str(date_range[0])} / {str(date_range[-1])}'
+
 
     def on_cancel(self, instance, value):
-        self.root.ids.date_label.text = "you clicked cancel"
-
-    #def use(self, instance, value, date_range, on_save, on_cancel):
-    #    if on_save == True:
-    #        self.root.ids.date_label.text = f'{str(date_range[0])} / {str(date_range[-1])}'
-    #        eventos = {"2000-12-27": "cumple nieto cristian"}
-    #        evento = self.root.ids.date_name.text
-    #        self.root.ids.date_name.text = ""
-    #        eventos[value] = evento
-    #    elif on_cancel == True:
-    #        self.root.ids.date_label.text = "you clicked cancel"
-    #    else:
-    #        self.root.ids.date_label.text = "error"
+        self.root.get_screen('CalendarScreen').ids.date_label.text = ""
 
     def show_date_picker(self):
-        #date_dialog = MDDatePicker(year=2000, month=2, day=14)
         date_dialog = MDDatePicker(mode="range")
-        #date_dialog.bind(use=self.use, on_save=self.on_save, on_cancel=self.on_cancel)
         date_dialog.bind(on_save=self.on_save, on_cancel=self.on_cancel)
         date_dialog.open()
 
+    # Get Time
+    def get_time(self, instance, time):
+        self.root.get_screen('CalendarScreen').ids.time_label.text = str(time)
 
-main().run()
+    # Cancel
+    def on_cancel(self, instance, time):
+        self.root.get_screen('CalendarScreen').ids.time_label.text = ""
+
+    def show_time_picker(self):
+        from datetime import datetime
+
+        # Define default time
+        default_time = datetime.strptime("4:20:00", '%H:%M:%S').time()
+
+        time_dialog = MDTimePicker()
+        # Set default Time
+        time_dialog.set_time(default_time)
+        time_dialog.bind(on_cancel=self.on_cancel, time=self.get_time)
+        time_dialog.open()
+
+
+    def save_event(self):
+        time = self.root.get_screen('CalendarScreen').ids.time_label.text
+        date = self.root.get_screen('CalendarScreen').ids.date_label.text
+        value = self.root.get_screen('CalendarScreen').ids.date_name.text
+        if date not in self.eventos.keys():
+            self.eventos[date] = {}
+            self.eventos[date][time] = value
+        else:
+            self.eventos[date][time] = value
+        print(self.eventos)
+
+        self.root.get_screen('CalendarScreen').ids.time_label.text = ""
+        self.root.get_screen('CalendarScreen').ids.date_label.text = ""
+        self.root.get_screen('CalendarScreen').ids.date_name.text = ""
+
+    def appnt_list(self):
+        for i in list(self.eventos.keys()):
+            for y in list(self.eventos[i].keys()):
+                items = ThreeLineListItem(text=self.eventos[i][y],
+                                          secondary_text=i,
+                                          tertiary_text=y)
+        self.root.get_screen('AppointmentScreen').ids.container.add_widget(items)
+
+    def notificar_recordatorio(self, eventos):
+        today = date.today()
+        now = datetime.now()
+        if today == eventos[today].key() and now == eventos[now].key():
+            show_notification("Recordatorio", eventos.eventos[today][now])
+        else:
+            pass
+
+# ---------------------------------------------------------------------------------------------------------
+if __name__ == "__main__":
+    NanaApp().run()
+    #NanaApp().show_notification("hey user", "take a break now")
+# ---------------------------------------------------------------------------------------------------------
